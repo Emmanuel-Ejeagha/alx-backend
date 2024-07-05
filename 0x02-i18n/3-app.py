174b6a2609d1ef8b871584261d9a967858a8a115
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-07-05 12:59:20 +0100

    task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""This module helps with internalization and localization of the application.
+
+    Config - contains configuration settings for the application.
+
+    get_locale:
+        Returns the preferred language for the user.
+
+    welcome_page:
+        Renders and returns the HTML welcome page.
+"""
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+
+class Config:
+    """config available languages for the babel
+        Language config
+        Locale config
+        Timezone config
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localselector
+def get_locale() -> str:
+    """Determines the preferred language for the user
+
+    Returns:
+        str: Preferred language for the user.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/", methods=["GET"])
+def welcome_page() -> str:
+    """Renders the HTML welcome page.
+
+    Returns:
+        str: Rendered HTMl welcome page.
+    """
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0')
