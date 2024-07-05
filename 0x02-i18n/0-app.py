de2a3077ff31fc225b43eb6bcfd7670e7ed48893
Emmanuel-Ejeagha <nuel6700@gmail.com>
2024-07-05 11:53:44 +0100

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,11 +8,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False, methods=['GET'])
+@app.route("/", strict_slashes=False, methods=["GET"])
 def welcome_page():
-    """Welcome to Holberton"""
+    """Welcome page for our application"""
+
     return render_template('0-index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0')
